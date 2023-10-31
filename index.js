dd0cb2b81d7edca4efc98fa5c7b82a91975db923
GabyTobar03 <to23001@esfe.agape.edu.sv>
2023-10-31 17:17:51 -0300

    Cree el archivo de notas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"//importando la libreria express.
 import dotenv from "dotenv"//importando la libreria dotenv.
 import usuarioRoute from "./routes/Usuario.routes.js"
+
 const app  = express(); //ejecutando la libreria.
 dotenv.config();
 const PORT = process.env.PORT || 3000;
