39505dd5e2cf82279d334ac3be96718bc74feea4
GabyTobar03 <to23001@esfe.agape.edu.sv>
2023-11-03 17:56:07 -0300

    sube a git

--- routes/Notas.routes.js
+++ routes/Notas.routes.js
@@ -4,24 +4,28 @@ import { Router } from "express";
 const router = Router();
 
 const notasFile = path.join(process.cwd(), 'data', 'Notas.json');
+const notas = ObtenerTodasLasNotas();
 
 // Ruta GET para obtener todas las notas
-router.get('/notas', (req, res) => {
-    const notas = ObtenerTodasLasNotas();
-    res.json({ notas });
+router.post('/obtener', (req, res) => {
+    const { userId} = req.body
+
+    // Filtra las notas que tienen el ID de usuario especificado
+    const notasRelacionadas = notas.filter(nota => nota.userId === userId);
+
+    return res.json({ notas: notasRelacionadas });
 });
 
 // Ruta para crear una nueva nota
 router.post('/notas', (req, res) => {
-    const { titulo, contenido } = req.body;
-    const notas = ObtenerTodasLasNotas();
+    const { userId,titulo, contenido } = req.body;
     
     const nuevaNota = {
+        userId,
         id: notas.length + 1,
         titulo,
         contenido
     };
-
     notas.push(nuevaNota);
 
     GuardarTodasLasNotas(notas);
@@ -38,7 +42,7 @@ function ObtenerTodasLasNotas() {
 
 // Función para guardar todas las notas en el archivo JSON
 function GuardarTodasLasNotas(notas) {
-    fs.writeFileSync(notasFile, JSON.stringify(notas, null, 2));
+    fs.writeFileSync(notasFile, JSON.stringify(notas));
 }
 
 const NotasRoute = router;
