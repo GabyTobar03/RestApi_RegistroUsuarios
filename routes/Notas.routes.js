3a86365280269280274c91579ad1cf27bb0d5a95
GabyTobar03 <to23001@esfe.agape.edu.sv>
2023-11-02 21:17:13 -0300

    cambie el codigo de notas.route para ver si se guardaban las notas

--- routes/Notas.routes.js
+++ routes/Notas.routes.js
@@ -5,18 +5,16 @@ const router = Router();
 
 const notasFile = path.join(process.cwd(), 'data', 'Notas.json');
 
-// Ruta GET para obtener todas las notas de un usuario por su ID
+// Ruta GET para obtener todas las notas
 router.get('/notas', (req, res) => {
-    const userId = req.params.userId;
-    const notas = ObtenerNotasPorUsuario(userId);
+    const notas = ObtenerTodasLasNotas();
     res.json({ notas });
 });
 
-// Ruta para crear una nueva nota para un usuario específico
+// Ruta para crear una nueva nota
 router.post('/notas', (req, res) => {
-    const userId = req.params.userId;
     const { titulo, contenido } = req.body;
-    const notas = ObtenerNotasPorUsuario(userId);
+    const notas = ObtenerTodasLasNotas();
     
     const nuevaNota = {
         id: notas.length + 1,
@@ -26,25 +24,21 @@ router.post('/notas', (req, res) => {
 
     notas.push(nuevaNota);
 
-    GuardarNotasPorUsuario(userId, notas);
+    GuardarTodasLasNotas(notas);
 
     return res.json({ message: 'Nota creada exitosamente', nota: nuevaNota });
 });
 
-// Función para cargar las notas de un usuario desde Notas.json
-function ObtenerNotasPorUsuario(userId) {
+// Función para cargar todas las notas desde Notas.json
+function ObtenerTodasLasNotas() {
     const contenido = fs.readFileSync(notasFile, 'UTF-8');
     const notas = JSON.parse(contenido);
-    return notas.filter(nota => nota.userId === userId);
+    return notas;
 }
 
-// Función para guardar las notas de un usuario en el archivo JSON
-function GuardarNotasPorUsuario(userId, notas) {
-    const contenido = fs.readFileSync(notasFile, 'UTF-8');
-    const todasLasNotas = JSON.parse(contenido);
-    const notasDelUsuario = todasLasNotas.filter(nota => nota.userId !== userId);
-    notasDelUsuario.push(...notas);
-    fs.writeFileSync(notasFile, JSON.stringify(notasDelUsuario, null, 2));
+// Función para guardar todas las notas en el archivo JSON
+function GuardarTodasLasNotas(notas) {
+    fs.writeFileSync(notasFile, JSON.stringify(notas, null, 2));
 }
 
 const NotasRoute = router;
